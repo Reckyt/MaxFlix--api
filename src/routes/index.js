78e2a2bb002bdb1d48ebccbdc04e6e99a116591b
Maxime <maxime.lombardo@gmail.com>
2020-10-21 16:19:40 +0200

    nouvelles routes pour les wanted et seen movies

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+
 const movies = require("./movies");
 const directors = require("./directors");
 const users = require("./users");
+const wishList = require("./wishList");
+const seenList = require("./seenList");
 
 const router = express.Router();
 
 router.use("/movies", movies);
 router.use("/directors", directors);
 router.use("/users", users);
+router.use("/wishList", wishList);
+router.use("/seenList", seenList);
 
 module.exports = router;
--- src/routes/seenList.js
+++ src/routes/seenList.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const connection = require("../config");
 
 router.get("/", (req, res) => {
-  connection.query("SELECT * from wished_movie", (err, results) => {
+  connection.query("SELECT * from seen_movie", (err, results) => {
     if (err) {
-      res.status(500).send("Error retrieving item in wishlist");
+      res.status(500).send("Error retrieving item in seenList");
     } else {
       res.json(results);
     }
@@ -16,11 +16,11 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   const idUrl = req.params.id;
   connection.query(
-    "SELECT * from wished_movie AS wm INNER JOIN movie AS m ON m.id_movie = wm.movie_id_movie WHERE wm.user_id = ? ",
+    "SELECT * from seen_movie AS sm INNER JOIN movie AS m ON m.id_movie = wm.movie_id_movie WHERE sm.user_id = ? ",
     idUrl,
     (err, results) => {
       if (err) {
-        res.status(500).send("Error retrieving item in wishlist");
+        res.status(500).send("Error retrieving item in seenList");
       } else {
         res.json(results);
       }
@@ -30,30 +30,25 @@ router.get("/:id", (req, res) => {
 
 router.put("/", (req, res) => {
   const formData = req.body;
-  connection.query(
-    "INSERT INTO wished_movie SET ?",
-    formData,
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Error saving item in wishlist");
-      } else {
-        res.sendStatus(200);
-      }
+  connection.query("INSERT INTO seen_movie SET ?", formData, (err, results) => {
+    if (err) {
+      res.status(500).send("Error saving item in seenList");
+    } else {
+      res.sendStatus(200);
     }
-  );
+  });
 });
 
 router.post("/:id", (req, res) => {
-  const formData = req.body.data.wishListBody;
+  const formData = req.body.data.seenListBody;
   connection.query(
-    "DELETE FROM wished_movie WHERE movie_id_movie = " +
+    "DELETE FROM seen_movie WHERE movie_id_movie = " +
       formData.movie_id_movie +
       " AND user_id = " +
       formData.user_id,
     (err) => {
       if (err) {
         res.status(500).send("Error deleting");
-        console.log(err);
       } else {
         res.sendStatus(200);
       }
