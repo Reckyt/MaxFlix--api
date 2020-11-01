ba836c0d3cc7ac07914dc2182061267511c614d4
Maxime <maxime.lombardo@gmail.com>
2020-11-01 18:07:53 +0100

    update credentiel

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST_SERVER,
+  user: process.env.DB_USER_SERVER,
+  password: process.env.DB_PASSWORD_SERVER,
+  database: process.env.DB_DATABASE_SERVER,
   insecureAuth: true,
 });
 module.exports = connection;
