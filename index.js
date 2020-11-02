7080b12acc6108e879c46513c56577a9e63a8600
Maxime <maxime.lombardo@gmail.com>
2020-11-02 10:16:09 +0100

    cors ok

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ const port = 3000;
 const bodyParser = require("body-parser");
 const api = require("./src/routes");
 
+const cors = require("cors");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use("/api", api);
 
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Header, Accept"
   );
-  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
   next();
 });
 
